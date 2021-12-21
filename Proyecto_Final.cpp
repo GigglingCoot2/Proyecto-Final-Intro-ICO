53fbf4d3afeedbc196a7c0b25d03ae5cf8fdb9dc
IreneMaldonado319 <irenemaldonado319@gmail.com>
2021-12-20 22:24:44 -0600

    Actualizacion de la funcion resta al Proyecto principal

--- Proyecto_Final.cpp
+++ Proyecto_Final.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 #include <stdio.h>
-
+#include "FResta.h"
 int main()
 {
     printf("\t\t ==Proyecto Final de Introduccion a ICO==\n");
+    //Funcion Resta
+    float x=0, y=0;
+    scanf_s("%f %f", &x, &y);
+    float resultado = restar(x,y);
 
+    printf_s("%.2f\n", resultado);
+
+    return 0;
     //compañeras favor de crear sus librerias en este archivo uwu
 }
