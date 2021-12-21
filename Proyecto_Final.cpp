7b6c2c6356ef022ac2b7ddce44aeb8d0f0a7691e
GigglingCoot2 <91902357+GigglingCoot2@users.noreply.github.com>
2021-12-20 19:24:58 -0600

    Add files via upload

---
+++ Proyecto_Final.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+    printf("\t\t ==Proyecto Final de Introduccion a ICO==\n");
+
+    //compañeras favor de crear sus librerias en este archivo uwu
+}
